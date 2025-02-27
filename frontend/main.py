dd7be311c83c47284b7bd6e9297e251469e59980
Max <mxbely98@gmail.com>
2025-02-27 15:25:04 +0200

    implement frontend choosing file and show points on the leaflet map

--- frontend/main.py
+++ frontend/main.py
@@ -1,11 +1,29 @@
-import dash
-from dash import html, dcc
+import dash_leaflet as dl
+from dash import Dash, html, dcc
 import base64
 import io
 from dash.dependencies import Input, Output
 import requests
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = Dash(__name__, suppress_callback_exceptions=True)
+
+
+def get_file_list():
+    response = requests.get("http://127.0.0.1:8000/api/tracks/files/")
+    if response.status_code == 200:
+        print("file_list", response.json())
+        return response.json()
+    return []
+
+
+def get_track_points(file_name):
+    response = requests.get(
+        f"http://127.0.0.1:8000/api/tracks/?file_name={file_name}"
+    )
+    if response.status_code == 200:
+        return response.json()
+    return []
+
 
 app.layout = html.Div(
     [
@@ -17,8 +35,37 @@ app.layout = html.Div(
         ),
         html.Div(id="output-data"),
         html.Div(id="upload-status"),
-    ]
+        html.Div(id="my-output"),
+        dcc.Dropdown(id="file-dropdown", placeholder="Select a file"),
+        dl.Map(
+            id="map",
+            children=[
+                dl.TileLayer(),
+            ],
+            center=[50, 30],
+            zoom=6,
+            style={"height": "50vh"},
+        ),
+    ],
 )
+
+
+@app.callback(Output("map", "children"), Input("file-dropdown", "value"))
+def update_map(selected_file):
+    if not selected_file:
+        return [dl.TileLayer()]
+
+    track_points = get_track_points(selected_file)
+
+    if not track_points:
+        return [dl.TileLayer()]
+
+    polyline = dl.Polyline(
+        positions=[[p["point_x"], p["point_y"]] for p in track_points],
+        color="red",
+    )
+
+    return [dl.TileLayer(), polyline]
 
 
 def parse_contents(contents):
@@ -31,13 +78,14 @@ def parse_contents(contents):
 
 @app.callback(
     Output("upload-status", "children"),
+    Output("file-dropdown", "options", allow_duplicate=True),
     Input("upload-data", "filename"),
     Input("upload-data", "contents"),
-    prevent_initial_call=True
+    prevent_initial_call='initial_duplicate',
 )
 def upload_file(filename, contents):
-    if contents is None:
-        return "No file uploaded"
+    if not contents or not filename:
+        return "", []
 
     file_io = parse_contents(contents)
     files = {"file": (filename, file_io, "application/csv")}
@@ -46,14 +94,31 @@ def upload_file(filename, contents):
         response = requests.post(
             "http://127.0.0.1:8000/api/tracks/upload/", files=files
         )
-        
+        dropdown_options = []
         if response.status_code == 201:
-            return "File uploaded successfully!"
+            file_list = get_file_list()
+            print(file_list)
+            dropdown_options = [{"label": f, "value": f} for f in file_list]
+
+            return "File uploaded successfully!", dropdown_options
+
+        elif response.status_code == 409:
+            return f"File {filename} already exists", dropdown_options
         else:
             return f"Error: {response.status_code}, {response.text}"
     except Exception as e:
         print(f"Request failed: {e}")
-        return "Error during upload"
+        return "Error during upload", dropdown_options
+
+
+@app.callback(
+        Output("file-dropdown", "options", allow_duplicate=True), 
+        Input("file-dropdown", "id"),
+        prevent_initial_call='initial_duplicate',
+    )
+def load_existing_files(_):
+    file_list = get_file_list()
+    return [{"label": f, "value": f} for f in file_list]
 
 
 if __name__ == "__main__":
